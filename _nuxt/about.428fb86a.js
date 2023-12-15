f7a3855391aaaf026d09db1ae482f5c6e85777c4
Tetsuya Saito <>
2023-12-16 03:31:46 +0900

    Updates

---
+++ _nuxt/about.428fb86a.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./entry.22a86c9e.js";const t={};function a(n,r){return o(),c("h1",null,"This is about Page")}const _=e(t,[["render",a]]);export{_ as default};
--- _nuxt/about.e28388e3.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./entry.f552a8c6.js";const t={};function a(n,r){return o(),c("h1",null,"This is about Page")}const _=e(t,[["render",a]]);export{_ as default};
