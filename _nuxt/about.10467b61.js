29f10b88d898127c40073307f6ed6b6cf016c92e
Tetsuya Saito <>
2023-12-17 22:18:55 +0900

    Updates

--- _nuxt/about.10467b61.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./entry.3c8a4287.js";const t={};function a(n,r){return o(),c("h1",null,"This is about Page")}const _=e(t,[["render",a]]);export{_ as default};
--- _nuxt/index.eb99fc6f.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./entry.3c8a4287.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
